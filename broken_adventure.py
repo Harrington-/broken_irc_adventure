ce93ffb7e702d36e180c3f44a0bb9d96bd8ce484
Blake <blake.harrington14@gmail.com>
2019-05-13 15:15:04 -0400

    Updated tables
    Added quest_table, region_table, and story_table

--- broken_adventure.py
+++ broken_adventure.py
@@ -13,7 +13,7 @@ from cloudbot.util import database
 from cloudbot.util.formatting import pluralize_auto, truncate
 from cloudbot.util.func_utils import call_with_args
 
-table = Table(
+user_table = Table(
     'broken_adventure_users',
     database.metadata,
     Column('nick', String),
@@ -21,24 +21,57 @@ table = Table(
     Column('deaths', String),
     Column('active_adventure', Boolean),
     Column('active_quest', Boolean),
-    PrimaryKeyConstraint('name')
+    PrimaryKeyConstraint('nick')
 )
 
+quest_table = Table(
+    "broken_adventure_quests",
+    database.metadata,
+    Column('name', String),
+    Column('region', Integer),
+    Column('desc', String),
+    Column('difficulty', Integer),
+    Column('id', Integer),
+    PrimaryKeyConstraint('id')
+)
+
+region_table = Table(
+    "broken_adventure_regions",
+    database.metadata,
+    Column("name", String),
+    Column("location", Integer),
+    Column("id", Integer),
+    PrimaryKeyConstraint('id')
+)
+
+story_table = Table(
+    "broken_adventure_story",
+    database.metadata,
+    Column("name", String),
+    Column("text", String),
+    Column("region", String),
+    Column("order", Integer),
+    Column("id", Integer),
+    PrimaryKeyConstraint('id')
+)
+
+#the command used to initialize a new adventure if one is not already active
 @hook.command("startadventure")
 def start_adventure(nick, db):
     adventure_details = initialize_adventure_details(nick, db)
     return adventure_details
 
+#the command used to stop an active adventure
 @hook.command("quitadventure")
 def quit_adventure(nick, db):
     adventure_details = forfeit_adventure(nick, db)
     return adventure_details
 
-
-    
+#this function will check the user table for any active adventures for the user and if none are found, then will update the table with the
+#new user or update existing user with a new adventure
 def initialize_adventure_details(nick):
     initialization_status = ""
-    if check_nick_for_existing_adventure(nick, db) ;
+    if check_nick_for_existing_adventure(nick, db)
         initialization_status = "There is already an ongoing adventure, you must finish or forfeit that one first before you may begin anew."
     else:
         initialization_status = "You have begun a new adventure. Please prepare for the quests that will come in time."
